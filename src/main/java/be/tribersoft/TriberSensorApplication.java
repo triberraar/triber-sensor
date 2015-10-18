caaafd9b52b595ff07042aefdf698ee48a4009fb
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-10-18 16:35:09 +0200

    initial checkin

---
+++ src/main/java/be/tribersoft/TriberSensorApplication.java
@@ -0,0 +1,12 @@
+package be.tribersoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriberSensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriberSensorApplication.class, args);
+    }
+}
