ec1e392d1ff68d2c3913684d8cc62c8a0f58ad39
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-10-24 16:47:26 +0200

    typefacade test

---
+++ src/test/java/be/tribersoft/sensor/domain/impl/type/TypeFacadeImplUpdateTest.java
@@ -0,0 +1,43 @@
+package be.tribersoft.sensor.domain.impl.type;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import be.tribersoft.sensor.domain.api.type.TypeUpdate;
+import be.tribersoft.sensor.domain.api.type.TypeUpdater;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TypeFacadeImplUpdateTest {
+	private static final long VERSION = 2L;
+	private static final String ID = "id";
+	@Mock
+	private TypeUpdater typeUpdater;
+	@Mock
+	private TypeEntity typeEntity;
+	@InjectMocks
+	private TypeFacadeImpl typeFacadeImpl;
+	@Mock
+	private TypeUpdate typeUpdate;
+	@Mock
+	private TypeRepositoryImpl TypeRepository;
+
+	@Before
+	public void setUp() {
+		when(TypeRepository.getByIdAndVersion(ID, VERSION)).thenReturn(typeEntity);
+	}
+
+	@Test
+	public void updatesTypeEntity() {
+		typeFacadeImpl.update(ID, VERSION, typeUpdate);
+
+		verify(typeUpdater).update(typeEntity, typeUpdate);
+	}
+
+}
