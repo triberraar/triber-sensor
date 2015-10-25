01abedf6293ffc7c6f7aa85b2f0bf3b97e427268
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-10-25 19:18:09 +0100

    concurrentmodificationexceptionhandler

---
+++ src/test/java/be/tribersoft/rest/ConcurrentModificationExceptionHandlerProcessValidationErrorTest.java
@@ -0,0 +1,44 @@
+package be.tribersoft.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import be.tribersoft.sensor.domain.api.exception.ConcurrentModificationException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConcurrentModificationExceptionHandlerProcessValidationErrorTest {
+
+	private static final String ERROR_MESSAGE = "validation.concurrent.modification";
+
+	@InjectMocks
+	private ConcurrentModificationExceptionHandler concurrentModificationExceptionHandler;
+
+	@Mock
+	private ErrorJsonFactory errorJsonFactory;
+
+	@Mock
+	private ErrorJson errorJson;
+
+	@Mock
+	private ConcurrentModificationException concurrentModificationException;
+
+	@Before
+	public void setUp() {
+		when(errorJsonFactory.create(ERROR_MESSAGE)).thenReturn(errorJson);
+	}
+
+	@Test
+	public void returnsAnErrorJson() {
+		ErrorJson processedErrorJson = concurrentModificationExceptionHandler.processValidationError(concurrentModificationException);
+
+		assertThat(processedErrorJson).isSameAs(errorJson);
+	}
+
+}
