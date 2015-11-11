353ade3db7ffc94b102b3831cf09e07e2a42d921
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-11-11 20:41:32 +0100

    api resource

---
+++ src/main/java/be/tribersoft/sensor/rest/api/ApiResource.java
@@ -0,0 +1,32 @@
+package be.tribersoft.sensor.rest.api;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.tribersoft.common.rest.IncorrectApiVersionException;
+
+@RestController
+@RequestMapping("/{apiVersion}")
+public class ApiResource {
+
+	@Value("${api.version}")
+	private String apiVersion;
+
+	@Inject
+	private ApiHateoasBuilder apiHateoasBuilder;
+
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	public Resource<ApiToJsonAdapter> get(@PathVariable("apiVersion") String apiVersion) {
+		if (!apiVersion.equals(this.apiVersion)) {
+			throw new IncorrectApiVersionException(this.apiVersion, apiVersion);
+		}
+		return apiHateoasBuilder.build(apiVersion);
+	}
+
+}
