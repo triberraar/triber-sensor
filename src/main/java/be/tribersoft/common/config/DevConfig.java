67c88f40facb6c87c5d3ea510ccb42bf5437e948
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-11-23 19:47:54 +0100

    devconfig bean

---
+++ src/main/java/be/tribersoft/common/config/DevConfig.java
@@ -0,0 +1,22 @@
+package be.tribersoft.common.config;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import be.tribersoft.sensor.domain.impl.event.EventJpaRepository;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Inject
+	private EventJpaRepository eventJpaRepository;
+
+	@PostConstruct
+	public void init() {
+		eventJpaRepository.deleteAll();
+	}
+}
